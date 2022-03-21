eed7e32314a8f1311a58e8ec351a2943152486fd
Midas Scheffers <midasscheffers@gmail.com>
2022-03-21 15:57:39 +0100

    [fix] fonts not displaying in chrome

--- age.js
+++ age.js
@@ -1,19 +1,11 @@
 
-
-
-function calculateAge(date) 
-{
+function calculateAge(date) {
   const now = new Date();
-  const diff = Math.abs(now - date );
+  const diff = Math.abs(now - date);
   const age = Math.floor(diff / (1000 * 60 * 60 * 24 * 365)); 
   return age
 }
 
-
-
-
 var text = document.getElementById("age")
-
 var age = calculateAge((new Date(2003, 9, 24)))
-
 text.innerHTML = age
