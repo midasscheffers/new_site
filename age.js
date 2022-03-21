fac3b4a957d9e75188a61e32d8513937ac99c429
midasscheffers_think <midasscheffers@gmail.com>
2022-03-21 15:04:34 +0100

    first comit

---
+++ age.js
@@ -0,0 +1,19 @@
+
+
+
+function calculateAge(date) 
+{
+  const now = new Date();
+  const diff = Math.abs(now - date );
+  const age = Math.floor(diff / (1000 * 60 * 60 * 24 * 365)); 
+  return age
+}
+
+
+
+
+var text = document.getElementById("age")
+
+var age = calculateAge((new Date(2003, 9, 24)))
+
+text.innerHTML = age
